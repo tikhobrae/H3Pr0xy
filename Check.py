33db9e7da9dd16d534a47724d009a8550abab174
Sir H3X <sayh3x@gmail.com>
2024-10-31 13:11:25 +0330

    Update add api

--- Check.py
+++ Check.py
@@ -1,2 +1,8 @@
-with open('AllProxy.txt', 'r') as file:
-    print(file.readline())
+def get():
+    with open('AllProxy.txt', 'r') as file:
+        get = file.readlines()
+        cleaned_lines = [line.strip() for line in get]
+    return cleaned_lines
+
+for i in get():
+    print(i)
